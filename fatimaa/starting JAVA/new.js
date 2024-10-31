ccae835dca5432bb6ae68c5ecb6b38abeae38354
nedacadmeygroup1 <2024nedpractice@gmail.com>
2024-10-30 23:05:06 -0800

    Add files via upload

---
+++ fatimaa/starting JAVA/new.js
@@ -0,0 +1,86 @@
+       // const studentname1 = "John"
+       // const studentname2 = "Aiza"
+       // const studentname3 = "Dora"
+       // let studentage1= "19"
+       // let studentage2= "20"
+       // let studentage3= "18"
+       // console.table([studentname1, studentname2, studentname3, studentage1, studentage2, studentage3])
+       
+       // console.log ( typeof course)
+       // console.log ( typeof group, age)
+
+       // let num = Number(studentname)
+       // console.log (typeof num);
+       // console.log(num);
+
+       // let str = String(studentname)
+       // console.log (typeof str);
+       // console.log(str);
+
+       // let char = String(age)
+       // console.log (typeof char);
+       // console.log(char);
+       
+//       let accountnumber ="caf";
+//       let accountmail = "caf@gmail"
+//       console.log (accountmail);
+//       accountmail = ("xyz");
+//       console.log (accountmail);   
+      
+      
+       // let a = true;
+       // console.log (typeof a);
+       // console.log (a);
+       // let valueNumber = Number(a)
+       // console.log (typeof valueNumber);
+       // console.log (valueNumber);
+
+       // let b = false;
+       // console.log (typeof b);
+       // console.log (b);
+       // let valueNumber2 = Number(b)
+       // console.log (typeof valueNumber2);
+       // console.log (valueNumber2);
+
+       // let c = 1;
+       // console.log (typeof c);
+       // console.log (c);
+       // let valueNumber3 = Boolean(c)
+       // console.log (typeof valueNumber3);
+       // console.log (valueNumber3);
+      
+       // let a=3;
+       // let b="3";
+       // if(a>b) {
+       //        console.log ("helloworld")  
+       // }
+       // else if (a=b) {
+       //        console.log("kuch bhi")
+       // }
+       // else if (a==b){
+       //        console.log("Leave me alone")
+       // }
+       // else if (a===b) {
+       //        console.log("welcome back")
+       // }
+       // else{
+       //        console.log("None")
+       // }
+       
+
+       let a="anuv jain";
+       let b="atif aslam";
+       let c="farhan saeed";
+
+       if (a==b) {
+              console.log ("perfect choice")
+       }
+       else if(a==c) {
+              console.log ("great choice")
+       }
+       else {
+              console.log(" good choice")
+       }
+       
+
+       
